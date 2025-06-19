181168f3f0f4d22e2f95d73fcb9393c7cc133ccb
ruzica-glavas <ruzi99@hotmail.it>
2025-06-19 16:57:59 +0200

    add: implementations for models of Vehicle and Maintenance and relation between them

--- src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/model/Maintenance.java
+++ src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/model/Maintenance.java
@@ -1,5 +1,81 @@
 package org.spring.carrozzeria.spring_carrozzeria_progetto_finale.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+@Entity
+@Table(name="maintenances")
 public class Maintenance {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @PastOrPresent
+    @NotNull(message = "Maintenance date cannot be null")
+    private LocalDate date;
+
+    @Lob
+    private String description;
+
+    @NotNull
+    @Min(value = 0, message = "Price cannot be null nor negative")
+    private Float price;
+
+    //Vehicle
+    @ManyToOne
+    @JoinColumn(name="vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Vehicle getVehicle() {
+        return this.vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
 }
--- src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/model/Vehicle.java
+++ src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/model/Vehicle.java
@@ -1,10 +1,19 @@
 package org.spring.carrozzeria.spring_carrozzeria_progetto_finale.model;
 
+import java.time.Year;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="vehicles")
@@ -12,4 +21,113 @@ public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotBlank(message = "Plate Number cannot be null nor empty or blank")
+    private String plateNumber;
+
+    @NotBlank(message = "Brand cannot be null nor empty or blank")
+    private String brand;
+
+    @NotNull //Può essere vuota
+    private String model;
+
+    @PastOrPresent
+    private Year year;
+
+    @Size(min=11, max=17, message="VIN must be between 11 and 17 charachters")
+    private String vin;
+
+    @Lob
+    private String photo;
+
+    @NotBlank(message="Owner name cannot be null nor empty or blank")
+    private String ownerName;
+
+    @NotBlank(message = "Owner phone cannnot be null nor empty or blank")
+    private String ownerPhone;
+
+    //Maintence
+    @OneToMany(mappedBy = "vehicle")
+    private List<Maintenance> maintenances;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlateNumber() {
+        return this.plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Year getYear() {
+        return this.year;
+    }
+
+    public void setYear(@PastOrPresent Year year) {
+        this.year = year;
+    }
+
+    public String getVin() {
+        return this.vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getPhoto() {
+        return this.photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getOwnerName() {
+        return this.ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerPhone() {
+        return this.ownerPhone;
+    }
+
+    public void setOwnerPhone(String ownerPhone) {
+        this.ownerPhone = ownerPhone;
+    }
+
+    public List<Maintenance> getMaintenances() {
+        return this.maintenances;
+    }
+
+    public void setMaintenances(List<Maintenance> maintenances) {
+        this.maintenances = maintenances;
+    }
+
 }
