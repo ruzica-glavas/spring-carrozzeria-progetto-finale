0b4916f66c369d107dab85c863d1a6bd2225f97b
ruzica-glavas <ruzi99@hotmail.it>
2025-06-19 15:35:18 +0200

    first commit

---
+++ src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/SpringCarrozzeriaProgettoFinaleApplication.java
@@ -0,0 +1,13 @@
+package org.spring.carrozzeria.spring_carrozzeria_progetto_finale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCarrozzeriaProgettoFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCarrozzeriaProgettoFinaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/SpringCarrozzeriaProgettoFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.carrozzeria.spring_carrozzeria_progetto_finale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCarrozzeriaProgettoFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
