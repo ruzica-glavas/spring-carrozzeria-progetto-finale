c7744dd1877b6e95084ae224d9a450e034cf4f78
ruzica-glavas <ruzi99@hotmail.it>
2025-06-19 17:02:55 +0200

    add: repositories

---
+++ src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/repository/MaintenanceRepository.java
@@ -0,0 +1,9 @@
+package org.spring.carrozzeria.spring_carrozzeria_progetto_finale.repository;
+
+import org.spring.carrozzeria.spring_carrozzeria_progetto_finale.model.Maintenance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintenanceRepository extends JpaRepository<Maintenance, Integer> {
+
+    
+} 
---
+++ src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package org.spring.carrozzeria.spring_carrozzeria_progetto_finale.repository;
+
+import org.spring.carrozzeria.spring_carrozzeria_progetto_finale.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+
+    
+} 
