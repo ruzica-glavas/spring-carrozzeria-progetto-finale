c558b6cb79b9aa3f4ac492bcefbbffe1102124d4
ruzica-glavas <ruzi99@hotmail.it>
2025-06-19 16:14:16 +0200

    add: created DB in mySql Workbench

--- src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/model/Vehicle.java
+++ src/main/java/org/spring/carrozzeria/spring_carrozzeria_progetto_finale/model/Vehicle.java
@@ -1,5 +1,15 @@
 package org.spring.carrozzeria.spring_carrozzeria_progetto_finale.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="vehicles")
 public class Vehicle {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
